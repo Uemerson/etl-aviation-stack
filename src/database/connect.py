e0872e45d2ae269ddebbb4ef2eabd0132aea0f76
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-09-09 22:14:46 -0300

    refactor: rename param connect function

--- src/database/connect.py
+++ src/database/connect.py
@@ -6,10 +6,10 @@
 import psycopg2
 
 
-def connect(conn):
+def connect(config_conn):
     """Connect to the PostgreSQL database server."""
     try:
-        with psycopg2.connect(**conn) as conn:
+        with psycopg2.connect(**config_conn) as conn:
             print("Connected to the PostgreSQL server.")
             return conn
     except psycopg2.DatabaseError:
